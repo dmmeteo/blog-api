3082eaa42375e6971598b379a317f09f3e049863
Dmitry Sichkar <dmmeteo@gmail.com>
2017-05-30 01:03:25 +0300

    CBV auth_required

--- post/views.py
+++ post/views.py
@@ -3,12 +3,20 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, ModelFormMixin
 from django.contrib.messages.views import SuccessMessageMixin
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from models import Post
 from forms import PostForm
 
 
-class PostCreateView(SuccessMessageMixin, CreateView):
+class LoginRequiredMixin(object):  # class to login_required - best practice
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+class PostCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     form_class = PostForm
     template_name = 'post/post_form.html'
     success_message = '%(title)s is created at %(created_at)s'
@@ -35,18 +43,24 @@ class PostListView(ListView):
         return qs
 
 
-class PostUpdateView(SuccessMessageMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'post/post_form.html'
     success_message = '%(title)s is updated'
 
     def get_success_url(self):
-        return reverse('post_list')
+        return reverse('post_detail', args=[self.object.pk])  # or kwargs={'pk': self.object.pk}
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostUpdateView, self).get_context_data(*args, **kwargs)
+        context['btn_url'] = self.get_success_url()
+        return context
 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
+    success_message = '%(title)s is deleted'
 
     def get_success_url(self):
         return reverse('post_list')
