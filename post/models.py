8db2fcf9dbd3679c82937f4c3bd2add98d513a6a
Dmitry Sichkar <dmmeteo@gmail.com>
2017-06-01 23:59:47 +0300

    CBV slug(start)

--- post/models.py
+++ post/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+    slug = models.SlugField(unique=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)  # "auto_now" means saved(updated) object
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)  # "auto_now_add" means initial(create) object
 
