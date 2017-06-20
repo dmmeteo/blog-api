cac97e50b9ff2ea77a0f311a7e03277df9d07028
Dmitry Sichkar <dmmeteo@gmail.com>
2017-06-21 01:07:25 +0300

    admin register category.model

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import Category
 
-# Register your models here.
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'updated', 'timestamp']
+    list_display_links = ['updated']
+    list_filter = ['updated', 'timestamp']
+    search_fields = ['title', 'description']
+    class Meta:
+        model = Category
+
+
+admin.site.register(Category, CategoryModelAdmin)
